1f7097798ea084979490a43b1cde8116db14a1d6
Marco Walther <marco@sonic.net>
2022-05-01 19:27:49 -0700

    Not sure, what happened here

--- sunray/motor.cpp
+++ sunray/motor.cpp
@@ -318,7 +318,7 @@ void Motor::run() {
     }
   }
     
-  sense();
+  sense();        
   
   if ((!resetMotorFault) && (checkFault())) {
     stopImmediately(true);
@@ -451,6 +451,8 @@ bool Motor::checkFault() {
 
 // measure motor currents
 void Motor::sense(){
+  if (millis() < nextSenseTime) return;
+  nextSenseTime = millis() + 20;
   motorDriver.getMotorCurrent(motorLeftSense, motorRightSense, motorMowSense);
   float lp = 0.995; // 0.9
   motorRightSenseLP = lp * motorRightSenseLP + (1.0-lp) * motorRightSense;
